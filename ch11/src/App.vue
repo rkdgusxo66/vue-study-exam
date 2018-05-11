1e944f08e902da44bdabe77789b3f838dc53b71b
강현태 <ganghyeontae@ganghyeontaeui-MacBook-Pro.local>
2018-05-11 23:35:35 +0900

    ch11

---
+++ ch11/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Vue from 'vue'
+///////////////////////////////////////////////
+// register globally
+import MenuIcon from "vue-material-design-icons/menu.vue"
+import BitcoinIcon from "vue-material-design-icons/bitcoin.vue"
+Vue.component("menu-icon", MenuIcon)
+Vue.component("bitcoin-icon",BitcoinIcon)
+///////////////////////////////////////////////
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
